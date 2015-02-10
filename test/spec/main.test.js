d60d727c9fe86d2b5508b05d6f5a9a44bfbd6cbe
YOUR NAME <erinpagemd@yahoo.com>
2015-02-10 01:59:24 -0600

    experimented with testing. commented out failed attempts for reference

--- test/spec/main.test.js
+++ test/spec/main.test.js
@@ -1,5 +1,7 @@
 /* jshint mocha: true, expr: true, strict: false, undef: false */
 
+//remember! tests depend on other tests. data you put out should be cleared at the end of the test
+
 describe('test suite', function () {
   it('should assert true', function () {
     true.should.be.true;
@@ -62,35 +64,46 @@ describe('makeFriendDiv', function () {
 //test that it grabs the info... issue clears at the end
 //test that it sends the info
 //test that it clears the info
-// describe('sendForm', function () {
-//   it('should clear the values of the input fields', function(){
-//     $('#name').val('bob');
-//     $('#phone').val('555-555-5555');
-//     $('#email').val('bob@aol.com');
-//     $('#twitter').val('@bob');
-//     $('#photo').val('http:imgur.com/bob');
-//     sendForm({originalEvent: MouseEvent, type: "click"});
-//     expect('#name').val().to.equal(' ');
-//   });
-// });
+describe('sendForm', function () {
+  it('should clear the values of the input fields', function(){
+    // $('#name').val('bob');
+    // $('#phone').val('555-555-5555');
+    // $('#email').val('bob@aol.com');
+    // $('#twitter').val('@bob');
+    // $('#photo').val('http:imgur.com/bob');
+    // var event = {
+    //   originalEvent: MouseEvent,
+    //   type: "click"
+    // };
+    // var sentForm = sendForm(event);
+    // expect($("#name").val()).to.equal(' ');
+  });
+});
 
 describe('stringifyInputValues', function () {
   it('should produce a json string', function () {
-    //do stuff here
+    //set the values for the test
+    // $('#name').val('bob');
+    // $('#phone').val('555-555-5555');
+    // $('#email').val('bob@aol.com');
+    // $('#twitter').val('@bob');
+    // $('#photo').val('http:imgur.com/bob');
+    // var stringified = stringifyInputValues();
+    // expect(stringified).to.be('object');
   });
 });//end stringifyInputValues
 
-// describe('showForm', function () {
-//   it('should give a style attribute of "display: block" to the form', function () {
-//     //same issue as above accessing the style attribute of undefined!! b/c it is a jquery object??
-//   })
-// })
+describe('showForm', function () {
+  it('should give a style attribute of "display:block" to the form', function () {
+    //same issue as below accessing the style attribute of undefined!! b/c it is a jquery object??
+  })
+})
 
-// describe('hideForm', function () {
-//   it('should give a style attribute of "display: none" to the form', function () {
-//     //style='display:none'
-//     hideForm();
-//     var displayAttr = document.querySelector('#contactForm').style;
-//     expect(displayAttr).to.equal('none');
-//   });
-// });
+describe('hideForm', function () {
+  it('should give a style attribute of "display:none" to the form', function () {
+    //style='display:none'
+    // hideForm();
+    // var displayAttr = document.querySelector('#contactForm').style;
+    // expect(displayAttr).to.equal('none');
+  });
+});
