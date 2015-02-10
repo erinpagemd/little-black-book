b949afaaf2029fdfe1be4d4afb58a9ff6785ba60
YOUR NAME <erinpagemd@yahoo.com>
2015-02-09 20:56:37 -0600

    capturing info on form and able to console.log

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,32 @@
-//use dev tool for iife
+//use dev tool for iife??
+$(document).ready(initialize);
 
-//upon initialization, the form is hidden
-//hideForm();
+function initialize () {
+  //upon initialization, the form is hidden
+  hideForm();
+  //click 'add new contact' and unhide the form
+  $('#addContact').click(showForm);
+  //click on the submit button and send the form
+  $('#submit').click(sendForm);
+}
 
-//click 'add new contact' and unhide the form
-$('#addContact').click(showForm);
+function sendForm(event) {
+  event.preventDefault();
+  var $name = $('#name').val();
+  var $phone = $('#phone').val();
+  var $email = $('#email').val();
+  var $twitter = $('#twitter').val();
+  var $url = $('#photo').val();
+
+  console.log($name, $phone, $email, $twitter, $url);
+}
+
+//post the form
+  //grab all of the information
+  //post it to firebase
+  //clear the form
+  //hide the form
+  //add info to the contact list
 
 //show the form
 function showForm () {
@@ -22,7 +44,8 @@ function hideForm () {
 //new contact form
   //name, phone number, email, twitter handle, photo url
   //submit
-  //on submit contact for disappears
+  //on submit contact form disappears
+  //prevent default
   //POST to firebase
   //display on the page the new information
 
