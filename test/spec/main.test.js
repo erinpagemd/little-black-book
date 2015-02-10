2ab01fa83714ce7630aeff93facfa1a8c07c8455
YOUR NAME <erinpagemd@yahoo.com>
2015-02-10 00:37:40 -0600

    planned tests and make a structure

--- test/spec/main.test.js
+++ test/spec/main.test.js
@@ -5,7 +5,31 @@ describe('test suite', function () {
     true.should.be.true;
     false.should.be.false;
   });
-});
+});//end test suite
+
+describe('banishFriend', function () {
+  it('should delete the person from the view', function () {
+    //do stuff here
+  });
+});//end banishFriend
+
+describe('getData', function () {
+  it('should getData from firebase', function () {
+    //do stuff here
+  });
+});//end getData
+
+describe('loadFriend', function () {
+  it('should put a div in the view', function () {
+    //do stuff here
+  });
+});//end loadFriend
+
+describe('makeFriendDiv', function () {
+  it('should make a div with all contact input info', function () {
+    //do stuff here
+  });
+});//end makeFriendDiv
 
 //isn't reading on preventDefault()... issue pass it an event!?!
 //test that it grabs the info... issue clears at the end
@@ -21,8 +45,19 @@ describe('test suite', function () {
 //     sendForm({originalEvent: MouseEvent, type: "click"});
 //     expect('#name').val().to.equal(' ');
 //   });
-//
 // });
+
+describe('stringifyInputValues', function () {
+  it('should produce a json string', function () {
+    //do stuff here
+  });
+});//end stringifyInputValues
+
+// describe('showForm', function () {
+//   it('should give a style attribute of "display: block" to the form', function () {
+//     //same issue as above accessing the style attribute of undefined!! b/c it is a jquery object??
+//   })
+// })
 
 // describe('hideForm', function () {
 //   it('should give a style attribute of "display: none" to the form', function () {
@@ -32,9 +67,3 @@ describe('test suite', function () {
 //     expect(displayAttr).to.equal('none');
 //   });
 // });
-
-// describe('showForm', function () {
-//   it('should give a style attribute of "display: block" to the form', function () {
-//     //same issue as above accessing the style attribute of undefined!! b/c it is a jquery object??
-//   })
-// })
